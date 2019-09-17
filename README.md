aa89a6f75f371034c07605fe1e4ae2578f3b01dd
dlithio <dlithio@users.noreply.github.com>
2019-09-17 10:34:39 -0500

    Script to copy ssh keys to pi

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ansible_scripts
 A place for my personal ansible scripts.
+
+## Depends
+* ssh-copy-id (can use brew install on Mac)
---
+++ ssh_setup/copy_ssh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
+if [[ "${1:-}" == "" ]]; then
+  >&2 echo "Give target ip as command line argument"
+  exit 1
+fi
+
+PI_IP="$1"
+
+echo "When prompted, use the password:"
+echo "raspberry"
+#rsync -av ~/.ssh pi@"$PI_IP":~/
+#ssh-copy-id pi@"$PI_IP"
+ssh pi@"$PI_IP" "rm ~/.ssh/config"
